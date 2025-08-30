9e78621765cf176462f251c93330924490ee94bd
Debabrata Hazra <debabrata.hazra@gmail.com>
2025-08-30 17:54:41 +0530

    Loan Eligible UI App implemented
    Loan eligible application with browser UI and train multiple model and take best model and also use MLFlow for each model metrics performance

---
+++ api/user_api.py
@@ -0,0 +1,61 @@
+import joblib
+
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+
+import mlflow
+import mlflow.sklearn
+
+from flask import Blueprint, request, jsonify
+import os
+import joblib
+import numpy as np
+
+from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+
+# Get the directory of the current Python file
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+
+user_bp = Blueprint('user', __name__)
+
+@user_bp.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+@user_bp.route('/users', methods=['POST'])
+def create_user():
+    
+    data = request.json
+    user = User(username=data['username'], email=data['email'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.to_dict()), 201
+
+@user_bp.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify(user.to_dict())
+
+@user_bp.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+    data = request.json
+    user.username = data.get('username', user.username)
+    user.email = data.get('email', user.email)
+    db.session.commit()
+    return jsonify(user.to_dict())
+
+@user_bp.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return '', 204
